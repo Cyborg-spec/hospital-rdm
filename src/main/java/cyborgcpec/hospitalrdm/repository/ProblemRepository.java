e653e6b36ae8ef84dfa8ef90c4581b0634f15673
Cyborg-spec <73435074+Cyborg-spec@users.noreply.github.com>
2021-04-12 19:47:14 +0400

    security added

--- src/main/java/cyborgcpec/hospitalrdm/repository/ProblemRepository.java
+++ src/main/java/cyborgcpec/hospitalrdm/repository/ProblemRepository.java
@@ -12,7 +12,7 @@ public interface ProblemRepository extends JpaRepository<Problem,Long> {
     @Query(value = "select from problem where problem_name in :problemNames",nativeQuery = true)
     Set<Problem> findByProblemNames(@Param("problemNames") Set<String> problemNames);
 
-    @Query(value = "select * from problem p join patient_problem pp on p.problem_id = pp.problem_id and pp.patient_id=:patientId",nativeQuery = true)
+    @Query(value = "select * from problem p left join patient_problem pp on p.problem_id = pp.problem_id and pp.patient_id=:patientId",nativeQuery = true)
     Set<Problem> findByPatient(@Param("patientId") long patientId);
 
     Problem findByProblemName(String problemName);
